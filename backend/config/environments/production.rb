b69a5ecfa57ffa64303bdfc5345353d512eb7ab7
webzawa <35700309+webzawa@users.noreply.github.com>
2023-05-11 04:19:09 +0900

    リポジトリ名変更

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "test_rails_20230510_production"
+  # config.active_job.queue_name_prefix = "rails_nextjs_test_20230510_production"
 
   config.action_mailer.perform_caching = false
 
